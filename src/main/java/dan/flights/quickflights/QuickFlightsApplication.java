44d477ed1065af22c01622f6b1b624f0ebe942ac
Daniel Sousa <67061995+dansou99@users.noreply.github.com>
2024-04-25 18:46:06 +0100

    QuickFlights first commit

---
+++ src/main/java/dan/flights/quickflights/QuickFlightsApplication.java
@@ -0,0 +1,13 @@
+package dan.flights.quickflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickFlightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dan/flights/quickflights/QuickFlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.flights.quickflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickFlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
